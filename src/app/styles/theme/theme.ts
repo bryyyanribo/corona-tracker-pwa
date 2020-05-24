fcb7fa93bd9a17d6fb159bfa8efcb35f98551848
bryyyanribo <ribobryan@gmail.com>
2020-05-24 22:34:46 +0800

    Update Hex Codes

--- src/app/styles/theme/theme.ts
+++ src/app/styles/theme/theme.ts
@@ -6,39 +6,21 @@ export interface Theme {
 export const light: Theme = {
   name: 'light',
   properties: {
-    '--foreground-default': '#08090A',
-    '--foreground-secondary': '#41474D',
+    '--background-default': '#FFF',
+    '--background-tertiary': '#2B2D2F',
     '--foreground-tertiary': '#FFF',
     '--foreground-quaternary': '#F4FAFF',
-    '--foreground-light': '#41474D',
-
-    '--background-default': '#FFF',
-    '--background-secondary': '#A3B9CC',
-    '--background-tertiary': '#5C7D99',
-    '--background-light': '#FFFFFF',
-
-    '--primary-default': '#5DFDCB',
     '--primary-dark': '#000',
-    '--primary-light': '#B2FFE7',
   },
 };
 
 export const dark: Theme = {
   name: 'dark',
   properties: {
-    '--foreground-default': '#5C7D99',
-    '--foreground-secondary': '#A3B9CC',
-    '--foreground-tertiary': '#000',
-    '--foreground-quaternary': '#E5E5E5',
-    '--foreground-light': '#FFFFFF',
-
     '--background-default': '#797C80',
-    '--background-secondary': '#41474D',
     '--background-tertiary': '#08090A',
-    '--background-light': '#41474D',
-
-    '--primary-default': '#5DFDCB',
+    '--foreground-quaternary': '#E5E5E5',
+    '--foreground-tertiary': '#000',
     '--primary-dark': '#fff',
-    '--primary-light': '#B2FFE7'
   },
 };
